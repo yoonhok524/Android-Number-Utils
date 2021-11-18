8dae5068fd0c5760517e43578b31fa2b68355da7
Yoonho Aaron Kim <youknow.yoonho@gmail.com>
2021-11-19 00:55:26 +0900

    Float.toDollarText 개선

--- NumberUtils/app/src/main/java/com/aaron/utils/number/Floats.kt
+++ NumberUtils/app/src/main/java/com/aaron/utils/number/Floats.kt
@@ -3,11 +3,14 @@ package com.aaron.utils.number
 import kotlin.math.pow
 import kotlin.math.round
 
-fun Float?.toDollarText(prefix: String = "$", default: String = "-"): String {
+fun Float?.toDollarText(
+    prefix: String = "$",
+    default: String = "-",
+digit: Int = 2): String {
     return if (this == null) {
         default
     } else {
-        "${prefix}${decimalFormat.format(this)}"
+        "${prefix}${decimalFormat.format(this.round(digit))}"
     }
 }
 
